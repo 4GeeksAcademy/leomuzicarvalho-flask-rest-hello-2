b47f852a243230e306982ac294aecb1212876553
Leonardo Muzí <leomuzicarvalho@gmail.com>
2023-12-18 18:07:08 +0000

    add migration example / delete user model and add car model as an example

--- src/models.py
+++ src/models.py
@@ -2,18 +2,16 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
+class Car(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    brand = db.Column(db.String(120), unique=True, nullable=False)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<Car %r>' % self.brand
 
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
+            "brand": self.brand,
             # do not serialize the password, its a security breach
         }
